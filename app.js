a66f74f9c7de6004bb48c66bfe112cfed837af10
Rafhi Ardi <rafhi.dev@gmail.com>
2025-05-31 11:41:08 +0700

    Update default route response and enhance code comments for clarity Add example configuration file for commit wizard tool with API settings
    **Explanation:**
    1. Modified `app.js` to improve the default route response message and added clarifying comments for routes.
    2. Introduced `wizard-commit.example.json` as a template configuration file for API integration settings.

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// Set up the routes for CRUD operations
 app.use("/api/", crudRoutes);
 
+// default route
 app.get("/", (req, res) => {
-  res.send("API ready.");
+  res.send("API is ready to use...");
 });
 
 app.listen(port, () => {
